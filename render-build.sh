9500a9506cc7f7042ca5c1cb0bd98eb84e4f41c1
Visheshyadav999 <140492666+Visheshyadav999@users.noreply.github.com>
2025-06-22 15:22:43 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Tesseract and required libraries
-apt-get update && apt-get install -y tesseract-ocr
-
-# Optional: Install language packs if needed (like Hindi, etc.)
-# apt-get install -y tesseract-ocr-hin
