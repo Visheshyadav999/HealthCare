f19b3a6140b08eff30078a8df960cd057d0ed544
Visheshyadav999 <140492666+Visheshyadav999@users.noreply.github.com>
2025-06-22 14:58:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,4 +259,6 @@ def mood_chat():
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
